3f0f0b5340c1419c9ab96c80804a226b33fd4cf5
jielanglang <1430861671@qq.com>
2019-07-10 18:41:58 +0800

    --story=1000008 --user=解浪浪 fix: 修复在typescript 中使用vuetify 类型作物问题以及集成失败问题

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,79 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>open_in_new</v-icon>
-      </v-btn>
-    </v-toolbar>
+     <v-card class="hide-overflow">
+    <v-app-bar
+      absolute
+      color="#6A76AB"
+      dark
+      shrink-on-scroll
+      prominent
+      src="https://picsum.photos/1920/1080?random"
+      fade-img-on-scroll
+      scroll-target="#scrolling-techniques-3"
+    >
+      <template v-slot:img="{ props }">
+        <v-img
+          v-bind="props"
+          gradient="to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)"
+        ></v-img>
+      </template>
 
-    <v-content>
-      <HelloWorld/>
-    </v-content>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Title</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>search</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>favorite</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>more_vert</v-icon>
+      </v-btn>
+
+      <template v-slot:extension>
+        <v-tabs align-with-title>
+          <v-tab>Tab 1</v-tab>
+          <v-tab>Tab 2</v-tab>
+          <v-tab>Tab 3</v-tab>
+        </v-tabs>
+      </template>
+    </v-app-bar>
+    <v-sheet
+      id="scrolling-techniques-3"
+      class="scroll-y"
+      max-height="600"
+    >
+      <v-container style="height: 1000px;">
+        <HelloWorld style="margin-top:260px" />
+      </v-container>
+    </v-sheet>
+  </v-card>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import HelloWorld from "./components/HelloWorld";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     HelloWorld
   },
-  data () {
+  data() {
     return {
-      //
-    }
+      items: [
+        { title: "Click Me" },
+        { title: "Click Me" },
+        { title: "Click Me" },
+        { title: "Click Me 2" }
+      ]
+    };
   }
-}
+};
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,34 @@
 import Vue from 'vue';
-import './plugins/vuetify'
+// import './plugins/vuetify'
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
 
+
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+
+const opts:any = {
+  theme: {
+    primary: '#ee44aa',
+    secondary: '#424242',
+    accent: '#82B1FF',
+    error: '#FF5252',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FFC107'
+  },
+  iconfont: 'md'
+}
+Vue.use(Vuetify)
 // 支持IE11和Safari 9 添加垫片
 import 'babel-polyfill'
 
 Vue.config.productionTip = false;
 
 new Vue({
+  vuetify: new Vuetify(opts),
   router,
   store,
   render: (h) => h(App),
