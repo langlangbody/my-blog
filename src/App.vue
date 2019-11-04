a91ea08d3bc834f583ddb162ad26fec96eaa55a8
jielanglang <1430861671@qq.com>
2019-11-04 14:01:44 +0800

    高度调整

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,52 @@
 <template>
   <v-app>
-     <v-card class="hide-overflow">
-    <v-app-bar
-      absolute
-      color="#6A76AB"
-      dark
-      shrink-on-scroll
-      prominent
-      src="https://picsum.photos/1920/1080?random"
-      fade-img-on-scroll
-      scroll-target="#scrolling-techniques-3"
-    >
-      <template v-slot:img="{ props }">
-        <v-img
-          v-bind="props"
-          gradient="to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)"
-        ></v-img>
-      </template>
+    <v-card class="hide-overflow">
+      <v-app-bar
+        absolute
+        color="#6A76AB"
+        dark
+        shrink-on-scroll
+        prominent
+        src="https://picsum.photos/1920/1080?random"
+        fade-img-on-scroll
+        scroll-target="#scrolling-techniques-3"
+      >
+        <template v-slot:img="{ props }">
+          <v-img v-bind="props" gradient="to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)"></v-img>
+        </template>
 
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Title</v-toolbar-title>
+        <v-toolbar-title>Title</v-toolbar-title>
 
-      <v-spacer></v-spacer>
+        <v-spacer></v-spacer>
 
-      <v-btn icon>
-        <v-icon>search</v-icon>
-      </v-btn>
+        <v-btn icon>
+          <v-icon>search</v-icon>
+        </v-btn>
 
-      <v-btn icon>
-        <v-icon>favorite</v-icon>
-      </v-btn>
+        <v-btn icon>
+          <v-icon>favorite</v-icon>
+        </v-btn>
 
-      <v-btn icon>
-        <v-icon>more_vert</v-icon>
-      </v-btn>
+        <v-btn icon>
+          <v-icon>more_vert</v-icon>
+        </v-btn>
 
-      <template v-slot:extension>
-        <v-tabs align-with-title>
-          <v-tab>Tab 1</v-tab>
-          <v-tab>Tab 2</v-tab>
-          <v-tab>Tab 3</v-tab>
-        </v-tabs>
-      </template>
-    </v-app-bar>
-    <v-sheet
-      id="scrolling-techniques-3"
-      class="scroll-y"
-      max-height="600"
-    >
-      <v-container style="height: 1000px;">
-        <HelloWorld style="margin-top:260px" />
-      </v-container>
-    </v-sheet>
-  </v-card>
+        <template v-slot:extension>
+          <v-tabs align-with-title>
+            <v-tab>Tab 1</v-tab>
+            <v-tab>Tab 2</v-tab>
+            <v-tab>Tab 3</v-tab>
+          </v-tabs>
+        </template>
+      </v-app-bar>
+      <v-sheet id="scrolling-techniques-3">
+        <v-container>
+          <HelloWorld style="margin-top:260px" />
+        </v-container>
+      </v-sheet>
+    </v-card>
   </v-app>
 </template>
 
@@ -77,3 +70,10 @@ export default {
   }
 };
 </script>
+
+<style>
+.hide-overflow {
+  overflow: hidden;
+  height: 100%;
+}
+</style>
